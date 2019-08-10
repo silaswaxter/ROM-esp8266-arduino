67f0775e11b2b72c205b1b994565c7d3808edd11
SilasWaxter <46617979+SilasWaxter@users.noreply.github.com>
2019-08-10 14:36:59 -0700

    Add files via upload

--- ESPnow-STAmaster.ino
+++ ESPnow-STAmaster.ino
@@ -5,9 +5,9 @@ extern "C" {              //see https://embeddedartistry.com/blog/2017/4/4/exter
 
 #define WIFI_CHANNEL 1
 
-uint8_t masterMac[] = {0x84, 0x0D, 0x8E, 0x8A, 0x92, 0xCF};
-uint8_t slaveMac[] = {0x86, 0x0D, 0x8E, 0x8F, 0x46, 0x76};
-uint8_t key[16] = {0x77, 0x39, 0x7A, 0x24, 0x43, 0x26, 0x46, 0x29, 0x4A, 0x40, 0x4D, 0x63, 0x51, 0x66, 0x54, 0x6A};
+uint8_t hubMacAdd[] = {0x86, 0x0D, 0x8E, 0x8F, 0x46, 0x76};
+uint8_t s1MacAdd[] = {0x84, 0x0D, 0x8E, 0x8A, 0x92, 0xCF};
+uint8_t key[16] = {0x71, 0x39, 0x7A, 0x24, 0x43, 0x26, 0x46, 0x29, 0x4A, 0x40, 0x4D, 0x63, 0x51, 0x66, 0x54, 0x6A};
 
 #define w 0
 #define x 1
@@ -21,23 +21,19 @@ union SENSOR_DATA{
 void setup() {
   Serial.begin(115200);
   Serial.println("\n");
-  Serial.println("ESP_Now Master\n");
+  Serial.println("ESP_Now Master/Sensor\n");
   
   WiFi.mode(WIFI_STA);    //Station mode for esp-now sensor node
-  WiFi.disconnect();
-
-  Serial.printf("This mac: %s \n", WiFi.macAddress().c_str());
-  Serial.printf("Target mac: %02x%02x%02x%02x%02x%02x \n", slaveMac[0], slaveMac[1], slaveMac[2], slaveMac[3], slaveMac[4], slaveMac[5]);
-  Serial.printf("Wifi_Channel: %i\n", WIFI_CHANNEL);
+  WiFi.disconnect();  
+  initEspNowMaster();
 
   sens.q[w] = 1.234; sens.q[x] = 5.678; sens.q[y] = 9.012; sens.q[z] = 3.456;
   
-  initEspNowMaster();
-  
+  Serial.printf("This mac: %s \n", WiFi.macAddress().c_str()); 
   Serial.println("Setup done\n-------------------------------\n\n");
 }
 
 void loop() {
-  esp_now_send(slaveMac, sens.msgBuffer, sizeof(sens.msgBuffer));
-  Serial.printf("%f, %f, %f, %f \n\n", sens.q[w], sens.q[x], sens.q[y], sens.q[z]);
+  esp_now_send(hubMacAdd, sens.msgBuffer, sizeof(sens.msgBuffer));
+  delay(500);
 }
--- initEspNowMaster.ino
+++ initEspNowMaster.ino
@@ -1,18 +1,19 @@
 //https://www.espressif.com/sites/default/files/documentation/2c-esp8266_non_os_sdk_api_reference_en.pdf
 
 void initEspNowMaster(){
-  if (esp_now_init() != 0) {
+  if (esp_now_init() == 0){
+
+    esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {     //CALLBACK function called when SEND register activated
+      if (sendStatus == 0) Serial.print("send_cb, send done, status = SUCCESS \n");
+      else Serial.print("send_cb, send done, status = FAILED \n\n");
+    });
+    esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+    
+    esp_now_set_kok(key, 16);                     //set local key
+    esp_now_add_peer(hubMacAdd, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, key, 16);
+  }
+  else{
     Serial.println("`````ESP_Now init failed`````");
     ESP.restart();
   }
-  
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_set_kok(key, 16);
-  
-  esp_now_add_peer(slaveMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, key, 16);
-
-  esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {     //CALLBACK function called when SEND register activated
-      if (sendStatus == 0) Serial.print("send_cb, send done, status = SUCCESS \n");
-      else Serial.print("send_cb, send done, status = FAILED \n\n");
-  });
 }
